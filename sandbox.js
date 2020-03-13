dfbcc413f4f0b21ad34a880e05c6ab0e6eca54ab
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-13 17:34:00 +1000

    Add changing CSS styles

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,6 @@
-const link = document.querySelector('a');
-console.log(link.getAttribute('href'));
-link.setAttribute('href','https://www.caniuse.com');
-link.innerText = 'CanIUse.com';
-console.log(link.getAttribute('href'));
+const title = document.querySelector('h1');
 
-const msg = document.querySelector('p');
-console.log(msg.getAttribute('class'));
-msg.setAttribute('class', 'success');
-msg.setAttribute('style', 'color: green;');
-console.log(msg.getAttribute('class'));
+title.style.margin = '50px';
+title.style.color = 'crimson';
+title.style.fontSize = '60px';
+title.style.margin = '';
