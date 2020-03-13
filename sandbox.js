728190b85c88185a1859f5fec6893f4a7da77cc1
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-13 21:20:43 +1000

    Add creating and removing elements

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,18 @@
+const ul = document.querySelector('ul');
+
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
-    console.log('you clicked me');
+    //ul.innerHTML += '<li>added item</li>';
+    const li = document.createElement('li');
+    li.textContent = 'something new';
+    //ul.append(li);
+    ul.prepend(li);
 });
 
 const items = document.querySelectorAll('li');
 items.forEach(item => {
     item.addEventListener('click', e => {
-        e.target.style.textDecoration = 'line-through';
+        e.target.remove();
     });
 })
