3f6722ba3c54f825b6a68c3243448495fd3f06c4
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-13 17:28:22 +1000

    Add getting and setting attributes

--- sandbox.js
+++ sandbox.js
@@ -1,17 +1,11 @@
-// change inner text of a tag
-const para = document.querySelector('p');
-para.innerText = 'JS is awesome!';
+const link = document.querySelector('a');
+console.log(link.getAttribute('href'));
+link.setAttribute('href','https://www.caniuse.com');
+link.innerText = 'CanIUse.com';
+console.log(link.getAttribute('href'));
 
-const paras = document.querySelectorAll('p');
-paras.forEach(para => {
-    para.innerText += ' added text';
-});
-
-// change inner HTML of a tag
-const content = document.querySelector('.content');
-content.innerHTML += '<h2>This is a new H2</h2>';
-
-const people = ['mario', 'luigi', 'yoshi'];
-people.forEach(person => {
-    content.innerHTML += `<p>${person}</p>`;
-});
+const msg = document.querySelector('p');
+console.log(msg.getAttribute('class'));
+msg.setAttribute('class', 'success');
+msg.setAttribute('style', 'color: green;');
+console.log(msg.getAttribute('class'));
