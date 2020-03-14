a8d96755d91f80d50c8f4b7a875209d50796b63b
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-14 10:43:27 +1000

    Add regex value testing

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,22 @@
+/* https://regex101.com/ */
+
 const form = document.querySelector('.signup-form');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     console.log(form.username.value);
 })
+
+const username = 'alexxx';
+const regexPattern = /^[a-z]{6,}$/;
+
+// let regexResult = username.search(regexPattern);
+// console.log(regexResult); // -1 or 1
+
+let regexResult = regexPattern.test(username);
+if(regexResult){
+    console.log('regex test passed :)')
+} else {
+    console.log('regex test failed :(')
+}
+
