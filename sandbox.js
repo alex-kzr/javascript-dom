aa8edf92d7b79f0cca589d2f7b97bbc8e0dd872a
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-14 11:37:51 +1000

    Add keyboard events

--- sandbox.js
+++ sandbox.js
@@ -2,16 +2,25 @@
 
 const form = document.querySelector('.signup-form');
 const feedback = document.querySelector('div.feedback');
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     
     const username = form.username.value;
-    const usernamePattern = /^[a-zA-Z]{6,12}/;
-
+    
     if(usernamePattern.test(username)){
         feedback.textContent = 'that username is valid!';
     } else {
         feedback.textContent = 'username must contain letters only & be between 6 & 12 charcters long';
     }
 });
+
+// live feedback
+form.username.addEventListener('keyup', e => {
+    if(usernamePattern.test(e.target.value)){
+        e.target.setAttribute('class', 'success');
+    } else {
+        e.target.setAttribute('class', 'error');
+    }
+})
